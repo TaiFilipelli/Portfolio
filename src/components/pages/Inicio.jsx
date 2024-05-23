de56386e2ec5de73a54303df103922b71c95ab55
TaiFilipelli <taielfilipelli@gmail.com>
2024-05-23 00:24:06 -0300

    new animations, fixed fonts

--- src/components/pages/Inicio.jsx
+++ src/components/pages/Inicio.jsx
@@ -1,17 +1,18 @@
 import React from 'react'
-import About from './About';
 import TypeComponent from '../TypeComponent';
+import { Slide } from 'react-awesome-reveal';
 
 const Intro = () => {
   return (
     <main>
+      <Slide direction='up' delay={100}>
         <section className='flex justify-center flex-col font-normal text-2xl font-unisonBold ml-32 h-80vh'>
             <h1 className='text-white'>Hola,</h1> 
             <h1 className='text-white'> soy <span className='bg-gradient-to-r from-pink-800 to-yellow-500 rounded-md p-2 bg-clip-text text-transparent font-bold'>Taiel!</span></h1>
             <br />
             <TypeComponent/>
-            
         </section>
+      </Slide>
     </main>
   )
 }
