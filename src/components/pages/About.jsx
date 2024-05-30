df6434a77ae5bce9a4d6be1226effbf5f2b7d41b
TaiFilipelli <taielfilipelli@gmail.com>
2024-05-30 20:49:34 -0300

    fade added to about section

--- src/components/pages/About.jsx
+++ src/components/pages/About.jsx
@@ -6,7 +6,8 @@ const About = () => {
       <Fade triggerOnce direction="left" className="items-left w-2/3 mb-6 my-10">
         <h1 className='font-unisonBoldIthalic'>About <span className="bg-gradient-to-r from-pink-800 to-yellow-500 bg-clip-text text-transparent">me</span></h1>
       </Fade>
-      <section className='rounded-md bg-black h-2/4 flex text-left w-2/3 p-10 flex-col'>
+      <Fade triggerOnce className="h-2/4 flex text-left w-2/3 flex-col" delay={250} duration={2000}>
+      <section className='rounded-md bg-black p-10'>
         <div className='flex justify-between'>
           <h1 className='font-unisonBold text-3xl mb-6'>Personal</h1>
           <UserFocus size={40}/> 
@@ -22,6 +23,7 @@ const About = () => {
         omnis fuga repudiandae quas reprehenderit! Aliquam odio porro dolor rerum animi nostrum ullam ipsum excepturi.</p>
         <hr />
       </section>
+      </Fade>
     </section>
   )
 }
