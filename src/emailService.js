c6688d1d42193e5b1ad8d6d197c99bb7cb81a2d4
TaiFilipelli <taielfilipelli@gmail.com>
2024-06-04 00:36:35 -0300

    changed credentials

--- src/emailService.js
+++ src/emailService.js
@@ -1,11 +1,11 @@
 import emailjs from "@emailjs/browser";
 
-const SERVICE_ID= import.meta.env.EMAILJS_SERVICE_ID;
-const TEMPLATE_ID=import.meta.env.EMAILJS_TEMPLATE_ID;
-const PUBLIC_KEY=import.meta.env.EMAILJS_PUBLIC_KEY;
+const SERVICE_ID = import.meta.env.EMAILJS_SERVICE_ID;
+const TEMPLATE_ID = import.meta.env.EMAILJS_TEMPLATE_ID;
+const PUBLIC_KEY = import.meta.env.EMAILJS_PUBLIC_KEY;
 
 const sendEmail=(templateParams)=>{
-    return emailjs.send(SERVICE_ID, TEMPLATE_ID, templateParams,PUBLIC_KEY)
+    return emailjs.send(SERVICE_ID, TEMPLATE_ID, templateParams, PUBLIC_KEY)
     .then((response)=>{
         console.log('EXITO!',response.status, response.text);
         return response;
