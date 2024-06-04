20c4e55a27c6330226599b27b49b096fcc4b7628
TaiFilipelli <taielfilipelli@gmail.com>
2024-06-03 23:23:23 -0300

    About "completed"

--- src/components/pages/About.jsx
+++ src/components/pages/About.jsx
@@ -1,4 +1,4 @@
-import { UserFocus,GraduationCap } from "phosphor-react";
+import { UserFocus,GraduationCap,TerminalWindow } from "phosphor-react";
 import { Fade } from "react-awesome-reveal";
 const About = () => {
   return (
@@ -12,15 +12,26 @@ const About = () => {
           <h1 className='font-unisonBold text-3xl mb-6'>Personal</h1>
           <UserFocus size={40}/> 
         </div>
-        <p className='text-xl pb-5'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aspernatur tenetur rerum necessitatibus facilis eaque soluta 
-        omnis fuga repudiandae quas reprehenderit! Aliquam odio porro dolor rerum animi nostrum ullam ipsum excepturi.</p>
+        <p className='text-xl pb-5'>Naci el 22 de Febrero del 2004. Mi interés por la programación y la informática surgió en la pandemia del 2020, donde gracias a varios cursos
+        (y mientras cursaba quinto año del secundario) empecé a capacitarme con cursos en linea y videos en YouTube. En un mes aprendí la estructura básica de una computadora y
+        los principios de la programación y la algoritmia. En 2022, unos 4 meses después de mi graduación, pude anotarme en la UTN a la carrera de Tecnicatura Universitaria en Programación.
+        Y desde ahi no paré.</p>
         <hr />
         <div className='flex justify-between my-6'>
           <h1 className='font-unisonBold text-3xl'>Educacion</h1>
           <GraduationCap size={35}/> 
         </div>
-        <p className='text-xl pb-5'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aspernatur tenetur rerum necessitatibus facilis eaque soluta 
-        omnis fuga repudiandae quas reprehenderit! Aliquam odio porro dolor rerum animi nostrum ullam ipsum excepturi.</p>
+        <p className='text-xl pb-5'>Mis estudios obligatorios los cumpli en la Escuela Secundaria Club Belgrano (2016-2021). La tecnincatura siguió inmediatamente después,
+        desde 2022 a 2024 en la Facultad Regional San Nicolas. Obviamente, gran parte de mis conocimientos se deben a aprendizajes extracurriculares que adquiri por mi cuenta gracias a cursos, 
+        videos, foros y mucho más.  </p>
+        <hr />
+        <div className='flex justify-between my-6'>
+          <h1 className='font-unisonBold text-3xl'>Atributtes</h1>
+          <TerminalWindow size={35}/> 
+        </div>
+        <p className='text-xl pb-5'>Soy una persona muy activa, con gran capacidad de trabajo en equipo y facilidad social. Suelo estar atento a los detalles y perfeccionar mis 
+        trabajos, asi como puedo adaptarme a cualquier tecnologia nueva que surja en el camino, gracias a mis ganas de aprender cosas nuevas constantemente. Soy responsable y puntual,
+        suelo ser muy exigente conmigo mismo y mi trabajo, me encanta estar todo el tiempo haciendo proyectos variados y poder ampliar mis conocimientos!</p>
         <hr />
       </section>
       </Fade>
