7a251daa4c5ac688d7a02ed593f84f2194da48c1
TaiFilipelli <taielfilipelli@gmail.com>
2024-06-10 20:38:44 -0300

    hold up, i'm cooking the responsive

--- src/components/pages/About.jsx
+++ src/components/pages/About.jsx
@@ -2,11 +2,11 @@ import { UserFocus,GraduationCap,TerminalWindow } from "phosphor-react";
 import { Fade } from "react-awesome-reveal";
 const About = () => {
   return (
-    <section className="flex justify-center items-center flex-col text-white">
+    <section className="projects-section flex justify-center items-center flex-col text-white">
       <Fade triggerOnce direction="left" className="items-left w-2/3 mb-6 my-10">
         <h1 className='font-unisonBoldIthalic'>About <span className="bg-gradient-to-r from-pink-800 to-yellow-500 bg-clip-text text-transparent">me</span></h1>
       </Fade>
-      <Fade triggerOnce className="h-2/4 flex text-left w-2/3 flex-col" delay={250} duration={2000}>
+      <Fade triggerOnce className="about-container h-2/4 flex text-left w-2/3 flex-col" delay={250} duration={2000}>
       <section className='rounded-md bg-black p-10'>
         <div className='flex justify-between'>
           <h1 className='font-unisonBold text-3xl mb-6'>Personal</h1>
