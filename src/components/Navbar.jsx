66e3495c6e0e85482d463362ed0824abbc7c3055
TaiFilipelli <taielfilipelli@gmail.com>
2024-06-10 22:19:47 -0300

    Responsive Inicio and Navbar!

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,13 +3,13 @@ import { Link } from 'wouter'
 import { Code } from 'phosphor-react'
 const Navbar = () => {
   return (
-    <nav className='bg-transparent p-4 w-full h-20 flex '>
-      <div className='container mx-auto flex justify-between gap-40 items-center'>
+    <nav className='bg-transparent p-4 w-full h-20 flex flex-row max-[733px]:w-1/3 max-[571px]:flex-col'>
+      <div className='container mx-auto flex justify-between gap-40 items-center max-[731px]:gap-4 max-[571px]:flex-col max-[571px]:w-screen'>
         <div className='text-white text-xl flex justify-between'>
           <Code size={28} className='mr-4'/>
           <Link to='/'><p className='font-unisonBold cursor-pointer'>Filipelli</p></Link>
         </div>
-        <div className='space-x-4 flex gap-8'>
+        <div className='space-x-4 flex gap-8 max-[768px]:gap-2 max-[571px]:flex-col max-[571px]:space-x-0 max-[571px]:pl-6'>
           <Link to='/about' className='text-white hover:text-slate-600 font-unisonLight'>About</Link>
           <Link to='/projects' className='text-white hover:text-slate-600 font-unisonLight'>Projects</Link>
           <Link to='/contact' className='text-white hover:text-slate-600 font-unisonLight'>Contact</Link>
--- src/components/pages/Inicio.jsx
+++ src/components/pages/Inicio.jsx
@@ -6,7 +6,7 @@ const Intro = () => {
   return (
     <main>
       <Slide direction='up' delay={100}>
-        <section className='flex justify-center flex-col font-normal text-2xl font-unisonBold ml-32 h-80vh'>
+        <section className='flex justify-center flex-col font-normal text-2xl font-unisonBold ml-32 h-80vh max-[768px]:text-xl max-[768px]:m-5 max-[311px]:text-base max-[574px]:h-[70vh]'>
             <h1 className='text-white'>Hi,</h1> 
             <h1 className='text-white'> i am <span className='bg-gradient-to-r from-pink-800 to-yellow-500 rounded-md p-2 bg-clip-text text-transparent font-bold'>Taiel!</span></h1>
             <br />
