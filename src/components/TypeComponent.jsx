ce2799bb5563fda44297b26ec2d55fb05fd785e8
TaiFilipelli <taielfilipelli@gmail.com>
2024-06-12 00:08:26 -0300

    one or two things added to type component

--- src/components/TypeComponent.jsx
+++ src/components/TypeComponent.jsx
@@ -3,7 +3,7 @@ import { TypeAnimation } from "react-type-animation"
 const TypeComponent = () => {
   return (
     <TypeAnimation
-        sequence={["FullStack Developer",1000,"Freelancer",1000,"Student",1000]}
+        sequence={["FullStack Developer",1000,"Freelancer",1000,"Videogame enjoyer",1000,"Student",1000]}
         wrapper="span"
         repeat={Infinity}
         style={{display:'inline-block',fontSize:'1.2em',fontFamily:'Unison Pro Bold Italic',color:'white'}}
