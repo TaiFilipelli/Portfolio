fa7c9f6516f78e175d57e0233afcbd2ebe0adb10
TaiFilipelli <taielfilipelli@gmail.com>
2025-02-27 20:16:33 -0300

    new project: Tech-cave

--- src/components/Timeline.jsx
+++ src/components/Timeline.jsx
@@ -37,19 +37,7 @@ const Timeline = () => {
           </div>
           <a href="https://matelibre.vercel.app" target="_blank"><img src='/MateLibre.png' alt="Preview Mate Libre"/></a>
         </VerticalTimelineElement>
-        {/* <VerticalTimelineElement className="vertical-timeline-element--education" date="2024-Today" contentArrowStyle={{ borderRight: '7px solid black' }} icon={<Snowflake/>}
-          iconStyle={{backgroundColor:'white',color:'#000'}} contentStyle={{backgroundColor:'#000',boxShadow:'none'}}>
-          <h1 className="text-white font-bold text-2xl font-unisonBold">Mateify</h1>
-          <h4 className='py-2'>One of my newest projects: a landing page of a fictional company called Mateify. It is an Angular project, with pure css and
-            with the goal of display information in a more design-oriented way. It is a small project, but it gives me the opportunity of showcase in a better way
-            my knowledges from Angular and my design & coding skills.
-          </h4>
-          <div className='w-full gap-3 flex flex-wrap mb-8 cursor-default'>
-            <p className='bg-white text-black py-1 px-3 rounded-xl'>Angular</p>
-            <p className='bg-white text-black py-1 px-3 rounded-xl'>CSS</p>
-          </div>
-          <a href="https://mateify.vercel.app" target='_blank' className="font-unisonBoldIthalic text-lg hover:underline"><img src="/Mateify.png" alt="Mateify" /></a>
-        </VerticalTimelineElement> */}
+
       <VerticalTimelineElement className="vertical-timeline-element--education" date="2024-2025" contentArrowStyle={{ borderRight: '7px solid black' }} icon={<Storefront/>}
           iconStyle={{backgroundColor:'white',color:'#000'}} contentStyle={{backgroundColor:'#000',boxShadow:'none'}}>
           <h1 className="text-white font-bold text-2xl font-unisonBold">PadelPoint Store</h1>
@@ -64,6 +52,21 @@ const Timeline = () => {
             <p className='bg-white text-black py-1 px-3 rounded-xl'>MySQL</p>
           </div>
           <a href="https://padel-point.vercel.app/" target='_blank' className="font-unisonBoldIthalic text-lg hover:underline"><img src="/PadelPoint.png" alt="PadelPoint Store"/></a>
+        </VerticalTimelineElement>
+        <VerticalTimelineElement className="vertical-timeline-element--education" date="In progress" contentArrowStyle={{ borderRight: '7px solid black' }} icon={<Snowflake/>}
+          iconStyle={{backgroundColor:'white',color:'#000'}} contentStyle={{backgroundColor:'#000',boxShadow:'none'}}>
+          <h1 className="text-white font-bold text-2xl font-unisonBold">Tech-Cave</h1>
+          <h4 className='py-2'>One of my newest projects: This one is an e-commerce, that it looks like any other. But there's a twist: this app doesn't have a dedicated database or API, 
+          instead, all of the information is stored in a .csv file of Google Sheets. Using axios, the app fetches the data from a sheet and displays it in the app. Every single change that 
+          we made in the Google Sheet it's inmediately reflected and displayed in the app. This way, some minor entrepreneurships can have it's own e-commerce without the need of a dedicated
+          API or database (not to mention the high costs that those things have).
+          </h4>
+          <div className='w-full gap-3 flex flex-wrap mb-8 cursor-default'>
+            <p className='bg-white text-black py-1 px-3 rounded-xl'>Next.js</p>
+            <p className='bg-white text-black py-1 px-3 rounded-xl'>TailwindCSS</p>
+            <p className='bg-white text-black py-1 px-3 rounded-xl'>Google Sheets</p>
+          </div>
+          <a href="https://tech-cave.vercel.app/" target='_blank' className="font-unisonBoldIthalic text-lg hover:underline"><img src="/Mateify.png" alt="Mateify" /></a>
         </VerticalTimelineElement>
         <VerticalTimelineElement iconStyle={{backgroundColor:'#000',color:'#fff'}} icon={<Moon/>} date="Work in progress...">
         </VerticalTimelineElement>
